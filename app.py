d0263a8494f5218dea6b77d1425abd814aa6d34b
Gaurav Chauhan <gauravschauhan1@gmail.com>
2016-07-30 19:59:59 +0530

    added webhook payload url endpoint

---
+++ app.py
@@ -0,0 +1,31 @@
+from flask import Flask
+from flask import request
+import requests
+from flask import jsonify
+
+app = Flask(__name__)
+
+DEBUG = True
+app.debug = DEBUG
+app.use_debugger = DEBUG
+
+TOKEN = '064968cdbc6a496571d814dd2769aa1d2ae137d2'
+
+@app.route("/email_hook", methods=['GET', 'POST'])
+def email_hook():
+    req_data = request.get_json()
+    try:
+        head_commit = req_data['head_commit']
+    except Exception as e:
+        head_commit = {}
+    head_commit_id = head_commit['id']
+    commit_url = 'https://api.github.com/repos/dotgc/zenforms/git/commits/%s'
+    diff_url = commit_url % head_commit_id
+    return jsonify({'diff': diff_url, 'token': TOKEN})
+
+def fetch_url(url):
+    resp = requests.get(url)
+
+
+if __name__ == "__main__":
+    app.run(host='localhost', port=5000)
